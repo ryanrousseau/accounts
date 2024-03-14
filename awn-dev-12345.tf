f3db3eb02c8576f523535f35d40beb70030fd8b4
Ryan Rousseau <r@ryanrousseau.com>
2024-03-14 09:22:39 -0500

    Remove some project connections from awn-dev-12345

--- awn-dev-12345.tf
+++ awn-dev-12345.tf
@@ -4,27 +4,7 @@ resource "octopusdeploy_tenant" "tenant" {
   tenant_tags           = ["Release Ring/Alpha", "Azure Account/Azure Demo Subscription", "Rancher Token/Octopus Deploy token"]
 
   project_environment {
-    environments = [ data.octopusdeploy_environments.environment.environments[0].id ]
+    environments = [ data.octopusdeploy_environments.production.environments[0].id ]
     project_id   = data.octopusdeploy_projects.all_octopub.projects[0].id
-  }
-
-  project_environment {
-    environments = [ data.octopusdeploy_environments.environment.environments[0].id ]
-    project_id   = data.octopusdeploy_projects.mysql.projects[0].id
-  }
-
-  project_environment {
-    environments = [ data.octopusdeploy_environments.environment.environments[0].id ]
-    project_id   = data.octopusdeploy_projects.octopub_audits.projects[0].id
-  }
-
-  project_environment {
-    environments = [ data.octopusdeploy_environments.environment.environments[0].id ]
-    project_id   = data.octopusdeploy_projects.octopub_frontend.projects[0].id
-  }
-
-  project_environment {
-    environments = [ data.octopusdeploy_environments.environment.environments[0].id ]
-    project_id   = data.octopusdeploy_projects.octopub_products.projects[0].id
   }
 }
--- data.tf
+++ data.tf
@@ -1,4 +1,12 @@
-data "octopusdeploy_environments" "environment" {
+data "octopusdeploy_environments" "development" {
+  name         = "Development"
+}
+
+data "octopusdeploy_environments" "test" {
+  name         = "Test"
+}
+
+data "octopusdeploy_environments" "production" {
   name         = "Production"
 }
 
