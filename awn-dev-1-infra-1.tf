9088634d14d2f2c42d22940f4979451f1bfff45e
Ryan Rousseau <r@ryanrousseau.com>
2024-03-18 14:17:59 -0500

    Add App B values for awn-dev-1-infra-1

--- awn-dev-1-infra-1.tf
+++ awn-dev-1-infra-1.tf
@@ -15,3 +15,35 @@ resource "octopusdeploy_tenant" "awn-dev-1-infra-1" {
 
   depends_on = [octopusdeploy_tag.awn-dev-1]
 }
+
+resource "octopusdeploy_tenant_project_variable" "awn-dev-1-infra-1-app-b-dev-aws-account" {
+  template_id = "00a1a103-2be1-4a6e-ba9f-993659fdb48c"
+  project_id = data.octopusdeploy_projects.app_b.projects[0].id
+  environment_id = data.octopusdeploy_environments.development.environments[0].id
+  tenant_id = octopusdeploy_tenant.awn-dev-1-infra-1.id
+  value = "Accounts-2923"
+}
+
+resource "octopusdeploy_tenant_project_variable" "awn-dev-1-infra-1-app-b-dev-aws-region" {
+  template_id = "299aaddd-34e7-4edb-a6a1-4e71e3ccb5b3"
+  project_id = data.octopusdeploy_projects.app_b.projects[0].id
+  environment_id = data.octopusdeploy_environments.development.environments[0].id
+  tenant_id = octopusdeploy_tenant.awn-dev-1-infra-1.id
+  value = "us-east-2"
+}
+
+resource "octopusdeploy_tenant_project_variable" "awn-dev-1-infra-1-app-b-dev-aws-cf-stack-name" {
+  template_id = "94ea2db8-b09b-4199-a0dd-892bb142c9bb"
+  project_id = data.octopusdeploy_projects.app_b.projects[0].id
+  environment_id = data.octopusdeploy_environments.development.environments[0].id
+  tenant_id = octopusdeploy_tenant.awn-dev-1-infra-1.id
+  value = "awn-dev-1-infra-1-app-b-stack"
+}
+
+resource "octopusdeploy_tenant_project_variable" "awn-dev-1-infra-1-app-b-dev-aws-cf-bucket-name" {
+  template_id = "7415748c-0ac0-4fc8-af42-66b147486960"
+  project_id = data.octopusdeploy_projects.app_b.projects[0].id
+  environment_id = data.octopusdeploy_environments.development.environments[0].id
+  tenant_id = octopusdeploy_tenant.awn-dev-1-infra-1.id
+  value = "awn-dev-1-infra-1-app-b-bucket"
+}
