494f955b413e39fff8ff6f98f320f27506d232b7
Ryan Rousseau <r@ryanrousseau.com>
2024-03-18 13:57:15 -0500

    Add order to tags

--- tags.tf
+++ tags.tf
@@ -6,180 +6,210 @@ resource "octopusdeploy_tag_set" "accounts" {
 resource "octopusdeploy_tag" "awn-dev-1" {
   color      = "#000000"
   name       = "awn-dev-1"
+  sort_order = 1
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-dev-2" {
   color      = "#000000"
   name       = "awn-dev-2"
+  sort_order = 2
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-dev-3" {
   color      = "#000000"
   name       = "awn-dev-3"
+  sort_order = 3
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-dev-4" {
   color      = "#000000"
   name       = "awn-dev-4"
+  sort_order = 4
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-dev-5" {
   color      = "#000000"
   name       = "awn-dev-5"
+  sort_order = 5
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-dev-6" {
   color      = "#000000"
   name       = "awn-dev-6"
+  sort_order = 6
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-dev-7" {
   color      = "#000000"
   name       = "awn-dev-7"
+  sort_order = 7
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-dev-8" {
   color      = "#000000"
   name       = "awn-dev-8"
+  sort_order = 8
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-dev-9" {
   color      = "#000000"
   name       = "awn-dev-9"
+  sort_order = 9
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-dev-10" {
   color      = "#000000"
   name       = "awn-dev-10"
+  sort_order = 10
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-dev-11" {
   color      = "#000000"
   name       = "awn-dev-11"
+  sort_order = 11
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-prod-12" {
   color      = "#000000"
   name       = "awn-prod-12"
+  sort_order = 12
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-prod-13" {
   color      = "#000000"
   name       = "awn-prod-13"
+  sort_order = 13
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-prod-14" {
   color      = "#000000"
   name       = "awn-prod-14"
+  sort_order = 14
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-prod-15" {
   color      = "#000000"
   name       = "awn-prod-15"
+  sort_order = 15
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-prod-16" {
   color      = "#000000"
   name       = "awn-prod-16"
+  sort_order = 16
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-prod-17" {
   color      = "#000000"
   name       = "awn-prod-17"
+  sort_order = 17
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-prod-18" {
   color      = "#000000"
   name       = "awn-prod-18"
+  sort_order = 18
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-prod-19" {
   color      = "#000000"
   name       = "awn-prod-19"
+  sort_order = 19
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-prod-20" {
   color      = "#000000"
   name       = "awn-prod-20"
+  sort_order = 20
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-prod-21" {
   color      = "#000000"
   name       = "awn-prod-21"
+  sort_order = 21
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-prod-22" {
   color      = "#000000"
   name       = "awn-prod-22"
+  sort_order = 22
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-prod-23" {
   color      = "#000000"
   name       = "awn-prod-23"
+  sort_order = 23
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-prod-24" {
   color      = "#000000"
   name       = "awn-prod-24"
+  sort_order = 24
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-test-25" {
   color      = "#000000"
   name       = "awn-test-25"
+  sort_order = 25
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-test-26" {
   color      = "#000000"
   name       = "awn-test-26"
+  sort_order = 26
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-test-27" {
   color      = "#000000"
   name       = "awn-test-27"
+  sort_order = 27
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-test-28" {
   color      = "#000000"
   name       = "awn-test-28"
+  sort_order = 28
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-test-29" {
   color      = "#000000"
   name       = "awn-test-29"
+  sort_order = 29
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
 resource "octopusdeploy_tag" "awn-test-30" {
   color      = "#000000"
   name       = "awn-test-30"
+  sort_order = 30
   tag_set_id = octopusdeploy_tag_set.accounts.id
 }
 
